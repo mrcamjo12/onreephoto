226db0d8d40fb35d0ecc3d352a4ef092fb9d37d2
mrcamjo12 <130695878+mrcamjo12@users.noreply.github.com>
2023-08-25 17:23:01 -0700

    firstpics

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,10 @@
 import { FaBars } from 'react-icons/fa'
 import { useGlobalContext } from './context'
-import PhotoSlide from './Carousel'
+import Airplane from './Photos/Home/airplane.jpg'
+import Hoop from './Photos/Home/hoop.jpg'
+import logo from './logo.svg'
+
+
 
 const Home = () => {
   const {openSidebar} = useGlobalContext()
@@ -10,6 +14,15 @@ const Home = () => {
         <FaBars />
       </button>
       
+      <div className='img-parent'>
+        <img src={logo} alt='onree photography' className='header-logo'></img>
+        <article className='home-img'>
+          <img src={Airplane} alt='airplane over LAX'></img>
+          <p>A plane over the skies heading into LAX</p>
+        </article>
+        <img src={Hoop} alt='hoop in'></img>
+
+      </div>
     </main>
   )
 }

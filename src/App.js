bd2646d6ffdc28d5e1a4785a285b875d9870efd7
mrcamjo12 <130695878+mrcamjo12@users.noreply.github.com>
2023-08-26 20:32:57 -0700

    scrollUpdates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Sidebar from './Sidebar'
 import Home from './Home'
+import logo from './logo.svg'
+
 
 
 function App() {
--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,24 @@
 import { FaBars } from 'react-icons/fa'
 import { useGlobalContext } from './context'
-import Airplane from './Photos/Home/airplane.jpg'
-import Hoop from './Photos/Home/hoop.jpg'
+import HomeImages from './images'
 import logo from './logo.svg'
-
-
 
 const Home = () => {
   const {openSidebar} = useGlobalContext()
   return (
     <main>
+
       <button className='sidebar-toggle' onClick={openSidebar}>
         <FaBars />
       </button>
       
-      <div className='img-parent'>
-        <img src={logo} alt='onree photography' className='header-logo'></img>
-        <article className='home-img'>
-          <img src={Airplane} alt='airplane over LAX'></img>
-          <p>A plane over the skies heading into LAX</p>
-        </article>
-        <img src={Hoop} alt='hoop in'></img>
+         <div className='container'>
+          <a href='/' className='main-logo'>
+            <img src={logo} alt='onree photography'></img>
+          </a>
+          <HomeImages className='home-contents'/>
+        </div>
 
-      </div>
     </main>
   )
 }
